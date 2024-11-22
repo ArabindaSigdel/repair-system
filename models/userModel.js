4cf6a112887b0c239ca750a3f6ef9dfe10d83185
Arbin <arbsigdel2060@gmail.com>
2024-11-22 10:23:05 +0545

    Renamed the file and modified the schema

---
+++ models/userModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+@.+\..+/, "Please enter a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [8, "Password must be at least 8 characters long"],
+    match: [
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      "Use strong password",
+    ],
+  },
+  role: {
+    type: String,
+    enum: ["Admin", "Mechanic", "Customer"],
+    default: "Customer",
+  },
+  phone: {
+    type: String,
+    required: [true, "Phone number is required"],
+    minlength: [10, "Phone number must be atleast 10 digits"],
+    maxlenth: [10, "Phone number must be atmost 10 digits "],
+  },
+  address: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  modifiedAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("User", userSchema);
