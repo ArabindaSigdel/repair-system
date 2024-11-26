b151a97bfd49de4a81be73f1e8f92c38b986a7db
Arabinda <arbsigdel2060@gmail.com>
2024-11-26 22:20:27 +0545

    Added contoller and routes to fetch vehicls

--- modules/vehicle/vehicle.routes.js
+++ modules/vehicle/vehicle.routes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const auth = require("../../middlewares/auth");
 const addVehicle = require("./controllers/addVehicle");
 const { model } = require("mongoose");
+const getVehicle = require("./controllers/getVehicle");
 
 const vehicleRouter = express.Router();
 
 vehicleRouter.use(auth);
 vehicleRouter.post("/addVehicle", addVehicle);
+vehicleRouter.get("/getVehicle", getVehicle);
 
 module.exports = vehicleRouter;
