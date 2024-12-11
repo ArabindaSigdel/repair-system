84de1d579ec6be47b17196d36888d56af688d5b9
Arbin <arbsigdel2060@gmail.com>
2024-12-11 09:17:12 +0545

    Repair Request Ticket Db model Added

--- models/user/repair.model.js
+++ models/user/repair.model.js
@@ -1,9 +1,25 @@
 const { default: mongoose } = require("mongoose");
 
-repairSchema = mongoose.Schema({
-  vehicle_id: {
-    type: mongoose.Schema.ObjectId,
-    ref: "vehicleModel",
-    required: [true, "Vehicle Id is required"],
+repairSchema = mongoose.Schema(
+  {
+    vehicle_id: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Vehicle",
+      required: [true, "Vehicle Id is required"],
+    },
+    workshop_id: {
+      type: mongoose.Schema.ObjectId,
+      required: [true, "Workshop Id is required"],
+    },
+    repair_description: {
+      type: String,
+      required: [true, "Description is required"],
+    },
+    status: {
+      type: String,
+      enum: ["Requested", "Accepted", "Pending", "Completed", "Re-opened"],
+      default: "Requested",
+    },
   },
-});
+  { timestamps: true }
+);
