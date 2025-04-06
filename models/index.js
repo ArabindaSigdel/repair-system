8465245a2f64fcd67f71f47ba14c453e87a7f28b
Arabinda <arbsigdel2060@gmail.com>
2025-04-06 18:49:29 +0545

    Moved /models/user/repair.model.js to /models/workshop/repair.model.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require("./user/user.model");
 const Vehicle = require("./user/vehicle.model");
-const Repair = require("./user/repair.model");
+const Repair = require("./workshop/repair.model");
 const AdminUser = require("./admin/adminUser.model");
 const Workshop = require("./workshop/workshop.model");
 
